974941138acfb41886b984a1c2daf2bef8726389
aloisclerc <alois.clerc@cengn.ca>
2021-06-10 13:56:21 -0400

    basic fizzbuzz function implemented and respective test passed

--- lab04/fizzbuzz_java/src/Fizzbuzz.java
+++ lab04/fizzbuzz_java/src/Fizzbuzz.java
@@ -1,3 +1,13 @@
 public class Fizzbuzz {
-    public 
+    public String fizzbuzz(int num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "FizzBuzz";
+        } else if (num % 3 == 0) {
+            return "Fizz";
+        } else if (num % 5 == 0) {
+            return "Buzz";
+        } else {
+            return num + "";
+        }
+    }
 }
--- lab04/fizzbuzz_java/test/FizzbuzzTest.java
+++ lab04/fizzbuzz_java/test/FizzbuzzTest.java
@@ -4,7 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FizzbuzzTest {
     @Test
-    public void fizz(){
-
+    public void fizzbuzzTest(){
+        Fizzbuzz f = new Fizzbuzz();
+        assertEquals("Fizz", f.fizzbuzz(6));
+        assertEquals("Buzz", f.fizzbuzz(20));
+        assertEquals("FizzBuzz", f.fizzbuzz(15));
     }
+
+    
 }
