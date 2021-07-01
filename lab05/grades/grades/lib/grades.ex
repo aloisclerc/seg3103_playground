18b241224ad9073da522dd5d499409972a23bcef
aloisclerc <alois.clerc@cengn.ca>
2021-07-01 15:07:19 -0400

    stubbed lab 5

--- lab05/grades/grades/lib/grades.ex
+++ lab05/grades/grades/lib/grades.ex
@@ -7,5 +7,23 @@ defmodule Grades do
   if it comes from the database, an external API or others.
   """
 
+  defmodule Calculator do
 
+    def percentage_grade(%{final: final, homework: homework, labs: labs, midterm: midterm}) do
+      list = ["10", "20", "30", "40", "50", "60", "70", "80", "87", "93", "100"]
+      Enum.random(list)
+    end
+
+    def letter_grade(%{final: final, homework: homework, labs: labs, midterm: midterm}) do
+      list = ["F", "E", "D", "D+", "C", "C+", "B", "B+", "A-", "A", "A+"]
+      Enum.random(list)
+
+    end
+
+    def numeric_grade(%{final: final, homework: homework, labs: labs, midterm: midterm}) do
+      list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      Enum.random(list)
+
+    end
+  end
 end
